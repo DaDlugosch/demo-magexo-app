63d3c014e27b122de5e75741d6480d8fde99638d
Damian Dlugosch <damian@damiandlugosch.com>
2022-10-26 10:02:04 +0200

    Bug fixes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <Header />
+  <Header/>
   <router-view/>
 </template>
 
 <script setup>
 import Header from '@/components/essentials/parts/Header.vue'
 
-// Check if Daisy color scheme is set to dark
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) document.documentElement.classList.toggle('dark')
+window.matchMedia('(prefers-color-scheme: dark)')?.matches && document.documentElement.classList.toggle('dark')
 </script>
